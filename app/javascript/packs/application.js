662584dbbf1b7ce2f5d5a80e8c7858c0e8bcfeb1
bargholz eimii <818_77_@ezweb.ne.jp>
2022-10-06 11:45:35 +0900

    Add bootstrap and button

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,11 +1,11 @@
 //= require rails-ujs
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+import Rails from "@rails/ujs";
+import Turbolinks from "turbolinks";
+import * as ActiveStorage from "@rails/activestorage";
+import "channels";
 import 'bootstrap';
 import '../stylesheets/application';
 import '../stylesheets/sample';
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+Rails.start();
+Turbolinks.start();
+ActiveStorage.start();
